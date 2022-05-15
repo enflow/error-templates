7a505b0888b68f2241432797e1c2760163b61c6a
Michel Bardelmeijer <m.bardelmeijer@gmail.com>
2022-05-15 16:48:01 +0200

    Add initial codes

---
+++ src/build.php
@@ -0,0 +1,17 @@
+<?php
+
+use Twig\Environment as TwigEnvironment;
+use Twig\Loader\FilesystemLoader as TwigFilesystemLoader;
+
+require '../vendor/autoload.php';
+
+$codes = require 'codes.php';
+
+$loader = new TwigFilesystemLoader(__DIR__ . '/../views');
+$twig = new TwigEnvironment($loader);
+
+foreach ($codes as $code) {
+    $rendered = $twig->render('codes/' . $code . '.twig', ['code' => $code]);
+
+    file_put_contents(__DIR__ . '/../dist/' . $code . '.html', $rendered);
+}
---
+++ src/symlink.php
@@ -0,0 +1,9 @@
+<?php
+
+require '../vendor/autoload.php';
+
+$codes = require 'codes.php';
+
+foreach ($codes as $code) {
+    exec('ln -s ../dist/' . $code . '.html ../custom_' . $code . '.html');
+}
