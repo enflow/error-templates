46ef576e96c02c5edf1ef2c09954ee3bd63dbc98
Michel Bardelmeijer <m.bardelmeijer@gmail.com>
2022-05-15 17:00:19 +0200

    Update symlink

--- src/symlink.php
+++ src/symlink.php
@@ -5,5 +5,5 @@ require '../vendor/autoload.php';
 $codes = require 'codes.php';
 
 foreach ($codes as $code) {
-    exec('ln -s ../dist/' . $code . '.html ../custom_' . $code . '.html');
+    exec('ln -s ../dist/' . $code . '.html ../../custom_' . $code . '.html');
 }
